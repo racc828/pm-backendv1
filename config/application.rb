fa9c6d0b72f83c20ee7039e77736729169581a8f
github username <github email address>
2017-08-30 13:39:13 -0400

    init

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Pmbackend
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+        config.middleware.insert_before 0, Rack::Cors do
+          allow do
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :delete, :patch, :options]
+       end
+     end
+     
+  end
+end
